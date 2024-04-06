70566c330098bf62dca875aa88f1b77e2d8dd6ea
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2024-04-07 01:45:02 +0600

    project besic setup

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,12 @@
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <div className="">
+                <h1>This is Error Page</h1>
+            </div>
+        </div>
+    );
+};
+
+export default ErrorPage;
